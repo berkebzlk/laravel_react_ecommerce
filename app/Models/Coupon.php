95e164e6cd5d41934c4b9975081cd3e7da9c3911
glock worthy <berkebozlak10@hotmail.com>
2024-08-20 01:13:49 +0300

    add model relationships

--- app/Models/Coupon.php
+++ app/Models/Coupon.php
@@ -2,10 +2,32 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Coupon extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name', 'discount', 'valid_until'];
+
+    /**
+     * Convert coupon name to uppercase
+     */
+    public function setNameAttribute($value) {
+        $this->attributes['name'] = Str::upper($value);
+    }
+
+        /**
+     * Check if coupon is valid
+     */
+    public function checkIfValid($value) {
+        if ($this->valid_until > Carbon::now()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -20,6 +20,13 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'address',
+        'city',
+        'zip_code',
+        'country',
+        'phone_number',
+        'profile_image',
+        'profile_completed',
     ];
 
     /**
@@ -44,4 +51,22 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function orders()
+    {
+        return $this->hasMany(Order::class)
+            ->with('products')
+            ->latest();
+    }
+
+    public function image_path() 
+    {
+        if($this->profile_image) 
+        {
+            return asset('storage/images/users/'.$this->profile_image);
+        } else {
+            return 'https://cdn.pixabay.com/photo/2017/11/10/05/48/user-2935527_640.png';
+        }
+    }
+
 }
